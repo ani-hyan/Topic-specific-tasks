aaaf33e0ec970b2a65729b4a6a20ac927789f254
ahakobyan <ahakobyan@griddynamics.com>
2024-01-11 16:52:18 +0400

    grouping by digits is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.example.grouping.GroupingByDigits;
+
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        int[] input = new int[]{3,44,108, 8, 51};
+        Map<Integer, List<String>> arr = new GroupingByDigits().groupByDigitNumbers(input);
 
+        arr.entrySet().forEach(System.out::println);
 
     }
-
 }
