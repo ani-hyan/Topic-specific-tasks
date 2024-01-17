7fca1e41028ff4837554ded56f30556a585fec48
ahakobyan <ahakobyan@griddynamics.com>
2024-01-17 14:48:20 +0400

    average

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,45 @@
 package org.example;
 
-import org.example.grouping.GroupingByDigits;
+import org.example.pubsub.average.RandomNumbersAverage;
+import org.example.pubsub.blockingqueue_impl.Publisher;
+import org.example.pubsub.blockingqueue_impl.Subscriber;
+import org.example.pubsub.pseudorandom.LockFreePseudoRandom;
 
-import java.util.List;
-import java.util.Map;
+
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
 
 public class Main {
-    public static void main(String[] args) {
-        int[] input = new int[]{3,44,108, 8, 51};
-        Map<Integer, List<String>> arr = new GroupingByDigits().groupByDigitNumbers(input);
+    public static void main(String[] args)  {
+//        BlockingQueue<String> messageQueue = new ArrayBlockingQueue<>(5);
+//
+//        Publisher p = new Publisher(messageQueue);
+//        Subscriber s = new Subscriber(messageQueue);
+//
+//        new Thread(p).start();
+//        new Thread(s).start();
+//        Thread publisher2 = new Thread(new Publisher(messageQueue));
+//        Thread subscriber2 = new Thread(new Subscriber(messageQueue));
+//
+//        publisher2.start();
+//        subscriber2.start();
 
-        arr.entrySet().forEach(System.out::println);
+//        LockFreePseudoRandom random = new LockFreePseudoRandom();
+//
+//
+//        for (int i = 1; i <= 10; i++) {
+//            int randomNumber = random.nextInt(6);
+//            System.out.println("Random Number " + i + ": " + randomNumber);
+//        }
+
+
+        System.out.println(RandomNumbersAverage.average());
 
     }
+
+
 }
